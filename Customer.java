ba32ad94cbb018e1451dee77a047fa653f0875a3
Ábner Neves <abnermneves@gmail.com>
2021-03-12 18:38:35 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -18,16 +18,12 @@ public class Customer {
    }
 
   public String statement() {
-     double totalAmount = 0;
-     int frequentRenterPoints = 0;
-     Enumeration rentals = _rentals.elements();
-     String result = "Rental Record for " + getName() + "\n";
-     while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    Enumeration rentals = _rentals.elements();
+    String result = "Rental Record for " + getName() + "\n";
+    while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -37,14 +33,15 @@ public class Customer {
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+            String.valueOf(each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
-     }
-     //add footer lines
-     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-     result += "You earned " + String.valueOf(frequentRenterPoints) +
-             " frequent renter points";
-     return result;
+    }
+
+    //add footer lines
+    result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "You earned " + String.valueOf(frequentRenterPoints) +
+            " frequent renter points";
+    return result;
    }
 }
