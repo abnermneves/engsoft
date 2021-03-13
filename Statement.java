f6672ce7b8fbb03e1bfc6d47cf48f78768d906e7
Ábner Neves <abnermneves@gmail.com>
2021-03-12 23:53:31 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,25 @@
-public class Statement {
-    
+import java.util.Enumeration;
+
+public abstract class Statement {
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = forWhom(aCustomer);
+        while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           //show figures for each rental
+           result += whichRental(each);
+        }
+        //add footer lines
+        result += customersCharge(aCustomer);
+        result += earnedPoints(aCustomer);
+        return result;
+    }
+
+    public abstract String forWhom(Customer aCustomer);
+
+    public abstract String whichRental(Rental aRental);
+
+    public abstract String customersCharge(Customer aCustomer);
+
+    public abstract String earnedPoints(Customer aCustomer);
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,36 +1,20 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = forWhom(aCustomer);
-        while (rentals.hasMoreElements()) {
-           Rental each = (Rental) rentals.nextElement();
-           //show figures for each rental
-           result += whichRental(each);
-        }
-        //add footer lines
-        result += customersCharge(aCustomer);
-        result += earnedPoints(aCustomer);
-        return result;
-    }
 
-    private String forWhom(Customer aCustomer) {
+    public String forWhom(Customer aCustomer) {
         return "Rental Record for " + aCustomer.getName() + "\n";
     }
 
-
-    private String whichRental(Rental aRental) {
+    public String whichRental(Rental aRental) {
         return "\t" + aRental.getMovie().getTitle()+ "\t" +
                String.valueOf(aRental.getCharge()) + "\n";
     }
     
-    private String customersCharge(Customer aCustomer) {
+    public String customersCharge(Customer aCustomer) {
         return "Amount owed is " +
                String.valueOf(aCustomer.getTotalCharge()) + "\n";
     }
     
-    private String earnedPoints(Customer aCustomer) {
+    public String earnedPoints(Customer aCustomer) {
         return "You earned " +
                String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
                " frequent renter points";
